89e06d50c8a98cc99b9a8761a76b360bcebb5ada
Charles X. Morrissey <me@charles-x.com>
2018-09-01 12:38:19 -0400

    disable productionTip

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
+import 'assets/css/app.css'
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import 'assets/css/app.css'
+// Vue App Config
+Vue.config.productionTip = false
 
 /* eslint-disable-next-line no-new */
 new Vue({
